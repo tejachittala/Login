b01d29dbe98b1ec895f366a00cbfc8fbc21d212f
Chittala Teja Venkata Phaneendra <91953749+Chittala-Teja-Venkata-Phaneendra@users.noreply.github.com>
2023-04-18 17:24:00 +0530

    Dynamic forms

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,11 +9,17 @@ export class ContactComponent {
 
   contactForm: FormGroup;
 
+  contactList: FormArray | undefined;
+
   constructor() {
     this.contactForm = new FormGroup (
       {
-      uname : new FormControl('Teja', Validators.compose([Validators.required,Validators.minLength(4), Validators.maxLength(8)])),
-      contact : new FormControl(),
+      uname : new FormControl("", Validators.compose([Validators.required,Validators.minLength(4), Validators.maxLength(8)])),
+      contact : new FormArray(
+        [
+          new FormControl("", Validators.required)
+        ]
+      ),
       address : new FormControl()
     }
     )
@@ -22,6 +28,14 @@ export class ContactComponent {
 
   show(fc:any) {
     console.log(fc);
+  }
+  AddContact() {
+    this.contactList = this.contactForm.controls['contact'] as FormArray;
+    this.contactList.push(new FormControl("", Validators.required))
+  }
+
+  remove(index: number) {
+    this.contactList?.removeAt(index);
   }
 
 }
